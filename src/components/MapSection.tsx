e3abc28051a3998ea7a9436b98fbcace1536caf0
Harish <harish.workx@gmail.com>
2026-09-18 15:53:56 +0530

    feat: add sitemap, robots.txt, and MapSection component

--- src/components/MapSection.tsx
+++ src/components/MapSection.tsx
@@ -55,13 +55,13 @@ export default function MapSection() {
           {/* Map embed */}
           <div className="rounded-2xl overflow-hidden shadow-lg min-h-[400px] border border-dark-100">
             <iframe
-              src="https://maps.google.com/maps?q=27525%20Puerta%20Real%20Ste%20300-140%20Mission%20Viejo%20CA%2092692&t=&z=14&ie=UTF8&iwloc=&output=embed"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3326350.0099134697!2d-122.5960285944551!3d35.494666200804254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fca54e0d30e35%3A0xab1aeb65d3aec52c!2sUltimate%20Green%20Electric%2C%20Inc.!5e0!3m2!1sen!2sin!4v1789726957376!5m2!1sen!2sin"
               width="100%"
               height="100%"
               style={{ border: 0, minHeight: '400px' }}
               allowFullScreen
               loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
+              referrerPolicy="strict-origin-when-cross-origin"
               title="Ultimate Green Electric location map"
             />
           </div>
